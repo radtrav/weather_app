463f2449d5d6122ca9938706ad386784ab17260f
Travis Young <t.w.young.ubc@gmail.com>
2017-02-22 15:55:35 -0800

    Implemented fetchWeather action creator

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '473896f3dadfcd414de0d1f3752c1e14';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  }
+}
